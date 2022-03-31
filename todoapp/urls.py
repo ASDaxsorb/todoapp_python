38e32835ae82840a69d2ca6e3dae8724bd57e6be
Axell Solis <axellsolisrubio@gmail.com>
2022-03-31 17:47:33 -0600

    Default url django todo

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("todo/", include("todo.urls")),
+    path("", include("todo.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
