7d36bfc34ab6a933429530f682bbea39f98ff433
Axell Solis <axellsolisrubio@gmail.com>
2022-03-31 18:35:42 -0600

    Set debug mode tu False

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--1i7t$)cjj(%p2t0b3_*^6d*a(_tsdr^bylwd0l7z*%0%ib(#o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
